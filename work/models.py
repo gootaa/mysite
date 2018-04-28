1723efd9e85fbc7ec3d40176748e5fe44de1140d
Unknown <abdommg4@fci.helwan.edu.eg>
2018-04-28 19:35:43 +0200

    Added models Project, Feedback
    also added them to admin site

--- work/models.py
+++ work/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdownify
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class Project(models.Model):
+    """
+    Represents a model for 
+    my personal projects
+    """
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, unique=True)
+    pic = models.ImageField(upload_to='projects/')
+    description = MarkdownxField()
+
+    @property 
+    def formatted_markdown(self):# This will be used in templates
+        return markdownify(self.description)
+
+    def get_absolute_url(self):
+        return reverse('project_detail', args=(self.slug,))
+
+    def __str__(self):
+        return self.name
+
+
+class Feedback(models.Model):
+    """
+    Represents client feedback
+    """
+    client_name = models.CharField(max_length=250)
+    client_pic = models.ImageField(upload_to='clients/', blank=True)
+    quote = models.TextField()
+
+    def __str__(self):
+        return self.client_name
