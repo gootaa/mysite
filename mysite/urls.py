9f72dc3e018f4c8eee169eed69f69cc4c0def7a1
Unknown <abdommg4@fci.helwan.edu.eg>
2018-05-06 05:50:28 +0200

    Added home and about views

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^admin/', admin.site.urls),
     url(r'^markdownx/', include('markdownx.urls')),
     url(r'^blog/', include('blog.urls')),
